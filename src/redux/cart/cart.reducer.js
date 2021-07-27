7607cd29f8e87c73ca947c04f453de7513a430ed
Ji Xu <xzji@amazon.com>
2021-07-27 13:55:41 -0700

    add cart utils to group items together

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,24 @@
 import { bindActionCreators } from 'redux';
+import { addItemToCart } from './cart.utils';
 
 import CartActionTypes from './cart.types';
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'TOGGLE_CART_HIDDEN':
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
